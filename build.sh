e27b694a982dabd1ddd387c4ff04e4fc0c5436dc
xBZZZZ <alanas.00@mail.ru>
2023-11-09 17:19:05 +0200

    don't need --gc-sections

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@
 set -e
 d=$(dirname "$0")
 cd -- "$d"
-exec ${CC:-cc} -O2 -Wl,--build-id=none,--hash-style=gnu,--strip-all,--gc-sections -o zip2gzip zip2gzip.c
+exec ${CC:-cc} -O2 -Wl,--build-id=none,--hash-style=gnu,--strip-all -o zip2gzip zip2gzip.c
